9d94e555e95ff676d3521aca8a2c0874557c7e58
Chris Massie <chris.massie@unity3d.com>
2021-01-25 20:06:09 +0000

    Update examples for 1.0.0-pre.2
    - Mirror of 8b17c45552afe7f015121785ab15941059ae352b

--- VR/Assets/Scripts/BubbleGun.cs
+++ VR/Assets/Scripts/BubbleGun.cs
@@ -16,40 +16,16 @@ public class BubbleGun : MonoBehaviour
     float m_TriggerHeldTime;
     bool m_TriggerDown;
 
-    void Start()
+    protected void Start()
     {
         m_InteractableBase = GetComponent<XRGrabInteractable>();
         m_Animator = GetComponent<Animator>();
-        m_InteractableBase.onSelectExited.AddListener(DroppedGun);
-        m_InteractableBase.onActivate.AddListener(TriggerPulled);
-        m_InteractableBase.onDeactivate.AddListener(TriggerReleased);
+        m_InteractableBase.selectExited.AddListener(DroppedGun);
+        m_InteractableBase.activated.AddListener(TriggerPulled);
+        m_InteractableBase.deactivated.AddListener(TriggerReleased);
     }
 
-    void TriggerReleased(XRBaseInteractor obj)
-    {
-        m_Animator.SetTrigger(k_AnimTriggerUp);
-        m_TriggerDown = false;
-        m_TriggerHeldTime = 0;
-        m_BubbleParticleSystem.Stop();
-    }
-
-    void TriggerPulled(XRBaseInteractor obj)
-    {
-        m_Animator.SetTrigger(k_AnimTriggerDown);
-        m_TriggerDown = true;
-    }
-
-    void DroppedGun(XRBaseInteractor obj)
-    {
-        // In case the gun is dropped while in use.
-        m_Animator.SetTrigger(k_AnimTriggerUp);
-
-        m_TriggerDown = false;
-        m_TriggerHeldTime = 0;
-        m_BubbleParticleSystem.Stop();
-    }
-
-    void Update()
+    protected void Update()
     {
         if (m_TriggerDown)
         {
@@ -63,6 +39,30 @@ public class BubbleGun : MonoBehaviour
                 }
             }
         }
+    }
+
+    void TriggerReleased(DeactivateEventArgs args)
+    {
+        m_Animator.SetTrigger(k_AnimTriggerUp);
+        m_TriggerDown = false;
+        m_TriggerHeldTime = 0f;
+        m_BubbleParticleSystem.Stop();
+    }
+
+    void TriggerPulled(ActivateEventArgs args)
+    {
+        m_Animator.SetTrigger(k_AnimTriggerDown);
+        m_TriggerDown = true;
+    }
+
+    void DroppedGun(SelectExitEventArgs args)
+    {
+        // In case the gun is dropped while in use.
+        m_Animator.SetTrigger(k_AnimTriggerUp);
+
+        m_TriggerDown = false;
+        m_TriggerHeldTime = 0f;
+        m_BubbleParticleSystem.Stop();
     }
 
     public void ShootEvent()
